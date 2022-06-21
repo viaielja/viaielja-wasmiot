256842f371eee440bada54a2e685f74c6116f878
Viljami Järvinen <viaielja@jyu.fi>
2022-06-21 09:23:18 +0300

    added function for module dependencies

---
+++ client/postDeviceDesc.js
@@ -0,0 +1,34 @@
+const fetch = require('node-fetch'); //used for 
+const manifest = require('./devicedescription.json'); //hard-coded json file with manifest of required functionality
+const fs = require('fs');
+
+
+
+/*
+sends a fetch POST request to a server containing manifest in the body
+*/
+(async function postFile() {
+    const rawResponse = await fetch('http://localhost:3000/.', {
+        method: 'POST',
+        headers: {
+      'Content-Type': 'text/html'
+        },
+        body: JSON.stringify(manifest)
+      }).then(data => console.log(data)); //log the response sent by the server
+
+  })();
+/*
+ //sends a fetch GET request to a server for a specific file
+ async function fetchFile(){
+  console.log('fetching files');
+  const response = await fetch('http://localhost:2000/.');
+  const fileStream = fs.createWriteStream("./files/received.wasm");
+  response.body.pipe(fileStream);
+ // response.body.on("error", reject);
+ //const data = await response.json();
+ //fileStream.on("finish", resolve);
+ // console.log(data);
+  }
+
+  
+//fetchFile();*/
--- fileserv/servapp.js
+++ fileserv/servapp.js
@@ -2,7 +2,9 @@ var http = require('http'),
     fileSystem = require('fs'),
     path = require('path');
 const { findSourceMap } = require('module');
-var modulemetadata = require("./files/modulemetadata.json");
+var CANDIDATEPACKAGE = [];
+var DEVICEMANIFEST;
+var DEVICEDESCRIPTION;
 
 
 http.createServer(function (request, response) {
@@ -10,25 +12,27 @@ http.createServer(function (request, response) {
     {
         let data = '';
         console.log('received POST');
-
-
-
         request.on('data', chunk => {
             data += chunk;
         });
+        //TODO: move handling after saving to local file in case of problems
         request.on('end', () => {
-
+            if (Object.keys(JSON.parse(data)).includes('architecture')) {
+                console.log(' --- this is a device description --- ');
+                fileSystem.writeFile('./files/devicedescription.json', data, function (err) {
+                    if (err) return console.log(err);
+                    console.log('--- data written to file devicedescription.json ---');
+                    response.end();
+                });
+            }
             //save sent json content of manifest to a json file
-            fileSystem.writeFile('./files/manifest.json', data, function (err) {
+            else fileSystem.writeFile('./files/manifest.json', data, function (err) {
                 if (err) return console.log(err);
                 console.log('data written to file manifest.json');
+                response.end(startSearch()); //todo: pass to checkModule
             });
-
-
-            response.end(digestManifest(JSON.parse(data)));
+            response.end();
         });
-
-
     }
 
     if (request.method === "GET" && request.url === "/") {
@@ -91,40 +95,233 @@ http.createServer(function (request, response) {
 
     }
 
-})
-    .listen(3000);
+}).listen(3000);
+
+const getDirectories = srcPath => fileSystem.readdirSync(srcPath).filter(file => fileSystem.statSync(path.join(srcPath, file)).isDirectory());
 
 
+//searches the server for modules that satisfy device description and manifest
+function startSearch() {
+    DEVICEDESCRIPTION = getDeviceDescription();
+    DEVICEMANIFEST = getManifest();
+    var modules = getDirectories("./modules"); //fetch name of the directories of every module
+    checkModules(DEVICEMANIFEST, DEVICEDESCRIPTION, modules);
+}
+
+function checkModules(deviceManifest, deviceDescription, modules) {
+    //go through all of the modules the server has
+    for (var i in modules) {
+        checkIndividualModule(deviceManifest, deviceDescription, modules[i]);
+    }
+}
+
+function checkIndividualModule(deviceManifest, deviceDescription, modulename) {
+    //TODO: handle superdependencies of modules
+    //TODO: handle loops in superdepency
+    //TODO: find most satisfying group of candidates 
+    var module = JSON.parse(getModuleJSON(modulename));
+    deviceDescription = JSON.parse(deviceDescription);
+    deviceManifest = JSON.parse(deviceManifest);
+    checkArchitecture(deviceDescription, module);
+    //checkPlatform(deviceDescription, module);
+    checkPeripherals(deviceDescription, module);
+    checkInterfaces(deviceManifest, module);
+    var deps = getModuleDependencies(module);
+    //checkSuperdependencies(module);
+    //TODO: check for superdependencies in module's metadata
+}
+
+//returns the json from a module based on the name
+function getModuleJSON(modulename) {
+    let startpath = path.join(__dirname, 'modules');
+    var truepath = path.join(startpath, modulename, 'modulemetadata.json');
+    //TODO: move to separate function for general use
+    return fileSystem.readFileSync(truepath, 'UTF-8', function (err, data) {
+        if (err) return console.log(err + " couldn't read the file!");
+        manifest = JSON.parse(data);
+    });
+}
+
+//checks Architecture and platform supported by module
+function checkArchitecture(deviceDescription, module) {
+    //TODO: check that device architecture and module architecture match
+    console.log(' ----- devicedescription -----');
+    console.log(deviceDescription);
+    console.log(' ----- modulemetadata -----');
+    console.log(module);
+    if (deviceDescription.architecture === module.architecture
+        && deviceDescription.platform
+        === module.platform) {
+        console.log("architecture and platform matches!");
+        return true;
+    }
+    return false;
+
+}
+
+//checks whether peripherals in device description and  module metadata match
+function checkPeripherals(deviceDescription, module) {
+    if (isSubset(deviceDescription.peripherals, module.peripherals)) {
+        console.log("Module peripherals match!");
+        return true;
+    }
+    else { console.log("module peripherals do not match"); return false; }
 
 
+}
+
+function checkInterfaces(deviceManifest, module) {
+    //checks if all required interfaces are offered by module
+    console.log(" -- offered interfaces from module -- ")
+    console.log(module.interfaces);
+    console.log(" -- interfaces required -- ");
+    console.log(getInterfaces(deviceManifest));
+    var fulfilledInterfaces = 0;
+    for (var i in getInterfaces(deviceManifest)) {
+        if (module.interfaces.includes(getInterfaces(deviceManifest)[i])) {
+            fulfilledInterfaces++;
+
+        }
+    }
+    if (fulfilledInterfaces == getInterfaces(deviceManifest).length) {
+        console.log("--- manifest interfaces satisfied --- ");
+        return true;
+    }
+    else {
+        console.log("--- manifest interfaces not satisfied --- ");
+        return false;
+    }
+}
+
+//reads the manifest sent by client
+function getDeviceDescription() {
+    //TODO: change to accept path of manifest
+    return fileSystem.readFileSync('./files/devicedescription.json', 'UTF-8', function (err, data) {
+        if (err) return console.log(err + " couldn't read the file!");
+
+        manifest = JSON.parse(data);
+    });
+}
+
+/*
+function checkModule(modulemeta) {
+    var modulematadata = modulemeta;
+    var deviceDescription = fileSystem.readFile('./files/devicedescription.json', 'UTF-8', function (err, data) {
+        if (err) return console.log(err);
+
+        console.log('---- Device description file has been read ----')
+        deviceDescription = JSON.parse(data);
+
+        checkCandidate(deviceDescription, modulemeta);
+    });
 
 
+}
+
+//check if candidate is suitable for the role on the device
+function checkCandidate(deviceDescription, moduleMetadata) {
+
+    //check if architecture and peripherals match
+    if (checkArchitectureAndPeripherals(deviceDescription, moduleMetadata)) {
+
+        console.log("--- module is suitable for device and platform --- ");
+        if (!CANDIDATEPACKAGE.includes(moduleMetadata.id)) {
+            CANDIDATEPACKAGE.push(moduleMetadata.id)
+            console.log("adding package to candidate modules");
+            console.log(CANDIDATEPACKAGE);
+
+        }
+        var manifest = getManifest();
+        console.log(manifest);
+        digestManifest(JSON.parse(manifest), moduleMetadata);
+
+    }
+
+}
+*/
+
+//reads the manifest sent by client
+function getManifest() {
+    //TODO: change to accept path of manifest
+    return fileSystem.readFileSync('./files/manifest.json', 'UTF-8', function (err, data) {
+        if (err) return console.log(err + " couldn't read the file!");
+
+        manifest = JSON.parse(data);
+    });
+}
+
+//returns an object containing dependencies of a module (superdependencies)
+function getModuleDependencies(moduleMetadata) {
+    var dependencies = moduleMetadata.dependencies;
+    if (dependencies === "") {
+        console.log("--- no dependencies found ---")
+        return undefined;
+    }
+    console.log(" --- dependecies of module --- ")
+    console.log(dependencies);
+    return dependencies;
+}
 
 
+//console.log(isSubset(["dht22" , "logitech_123"], ["dht22", "logitech_123", "networking"] ));
+//checks if one set is subset of another
+function isSubset(set, subset) {
+    if (subset == "") { return false };
+    console.log(set);
+    console.log("compared to ");
+    console.log(subset);
 
+    for (var i in subset) {
+        //console.log(i);
+        // console.log(set.includes(subset[i]));
+        if (!set.includes(subset[i])) {
+            console.log(set + " does not include :" + subset[i]);
+            return false;
+        }
 
-//
-function digestManifest(manifest) {
+    }
+    return true;
+    // if(set === subset) {console.log('exact'); return true};
+    /* if(!subset.every(isSub)){return false};
+     return true;
+     function isSub(value){
+         return set.includes(value);           
+     }*/
+
+}
+
+//handle manifest and matching of interfaces
+function digestManifest(manifest, moduleMetadata) {
     if (getObjects(manifest, 'roles', '') != []) {
         console.log("this is a device deployment manifest");
-        var manifestRoles = getObjects(manifest, 'interface', '');
+        var manifestRoles = getManifestRoles(manifest);
         var interfaces = getValues(manifestRoles, 'interface');
-        var moduleInterfaces = modulemetadata.interfaces;
-        matchInterfaces(moduleInterfaces, interfaces);
+        var moduleInterfaces = getModuleInterfaces(moduleMetadata);
+
+        //save candidate name with interfaces for later
+        if (matchInterfaces(moduleInterfaces, interfaces)) {
+
+            //TODO: check superdependencies
+            console.log("found requested interfaces in module")
+            fileSystem.appendFile('./files/solutionCandidates.txt', JSON.stringify(moduleMetadata.id) + ' : ' + moduleInterfaces, function (err) {
+                if (err) throw err;
+                console.log('Candidate module has been saved');
+            });
+        }
+        else return;
     }
 
     else { console.log("this is something else"); }
 }
 
+function getModuleInterfaces(moduleMetadata) {
+    return moduleMetadata.interfaces;
+}
 
-/* function digestModuleMetadata(modulemetadata){
-     if (getObjects(modulemetadata, 'id', '') != [] ){
-         var interfaces = modulemetadata.interfaces;
-         matchInterfaces(interfaces, manifest);
-     
-     }
- }
-*/
+function getManifestRoles(manifest) {
+    return getObjects(manifest, 'interface', '');
+}
+
 
 //checks if all required interfaces are offered by module
 function matchInterfaces(moduleInterfaces, manifestInterfaces) {
@@ -132,31 +329,28 @@ function matchInterfaces(moduleInterfaces, manifestInterfaces) {
     console.log(moduleInterfaces);
     console.log(" -- interfaces required -- ")
     console.log(manifestInterfaces);
+    var fulfilledInterfaces = 0;
     for (var i in manifestInterfaces) {
         if (moduleInterfaces.includes(manifestInterfaces[i])) {
-            //check if all modules match
+            fulfilledInterfaces++
+            console.log(fulfilledInterfaces);
         }
     }
-
-}
-
-
-/*    //separates each role into its own
-function separateRoles(manifest, interfaces) {
-    var roleObjects = [];
-    for (var interface in interfaces){
-    var name = "temperature_logging";
+    if (fulfilledInterfaces == manifestInterfaces.length) {
+        console.log("--- manifest interfaces satisfied --- ");
+        return true;
     }
-    return roleObjects;
+
+    else {
+        console.log("--- manifest interfaces not satisfied --- ");
+        return false;
+    }
 }
-*/
 
 //returns interfaces from the roles
-function getInterfaces(roles) {
-    return Object.keys(roles);
+function getInterfaces(deviceManifest) {
+    return getValues(deviceManifest.roles, "interface");
 }
-
-
 
 function matchRoles(clientManifest, modulemetadata) {
     var clientRoles = getRoles(clientManifest),
@@ -172,6 +366,7 @@ function matchRoles(clientManifest, modulemetadata) {
 function getRoles(manifest) {
     console.log('---- searching for roles ----')
     roles = [];
+    //TODO: change to .every
     for (var i in manifest.roles) {
         roles.push(manifest.roles[i]);
         console.log(roles[i]);
@@ -181,7 +376,43 @@ function getRoles(manifest) {
 }
 
 
+function getModulemetadata(dirPaths) {
+    let startpath = path.join(__dirname, 'modules');
+    for (var i in dirPaths) {
 
+        console.log(dirPaths[i]);
+        var moduleMetadata;
+        var truepath = path.join(startpath, dirPaths[i], 'modulemetadata.json');
+        //console.log(truepath);
+
+        //move to separate function for general use
+        var moduleMetadata = fileSystem.readFile(truepath, 'utf8', function (err, data) {
+            let metadata = data;
+            checkModule(JSON.parse(data));
+
+        });
+
+    }
+
+    //handels device description sent by device
+    function digestDeviceDescription(manifest, modulemetadata) {
+        // console.log(modulemetadata);
+        //TODO: handle device description
+        var deviceDescription;
+        var moduleMetadata;
+        deviceDescription = fileSystem.readFile('./files/devicedescription.json', 'UTF-8', function (err, data) {
+            if (err) return console.log(err);
+
+            console.log('---- file has been read ----')
+            deviceDescription = JSON.parse(data);
+            moduleMetadata = modulemetadata;
+
+        });
+    }
+
+
+
+}
 
 //compares two individual objects for their contents to assert equality
 function deepEqual(object1, object2) {
@@ -207,12 +438,6 @@ function deepEqual(object1, object2) {
 
 function isObject(object) {
     return object != null && typeof object === 'object';
-}
-
-
-//finds a matching keyword from json and prints the key + value
-function readManifest(manifest) {
-    //
 }
 
 
@@ -274,11 +499,25 @@ function lookUpKey(key, value) {
 
             console.log("found " + key + " with : " + value);
         }
-
     }
-
-
 }
+
+   //checks if architecture and peripherals of the module are suitable for the device
+/*function checkArchitectureAndPeripherals(deviceDescription, modulemetadata){
+    console.log('devicedescription -----');
+    console.log(deviceDescription);
+    console.log('modulemetadata -----');
+    console.log(modulemetadata);
+    //checking whether module architecture and peripheral list are satisfactory
+    if(deviceDescription.architecture === modulemetadata.architecture 
+        && isSubset(deviceDescription.peripherals ,modulemetadata.peripherals)){
+            
+       console.log("architecture and platform matches!");
+        return true;
+    }
+    return false;
+}
+*/
 
 
 
