237aaa4a55c88e84f3045b54abb9f37bde7cb848
Juho Tarkkanen <juaatark@jyu.fi>
2022-12-19 15:38:00 +0200

    Separate routing a bit

--- fileserv/server.js
+++ fileserv/server.js
@@ -4,39 +4,26 @@ var http = require('http'),
 
 const { chdir } = require('process');
 
+
+// Set working directory to this file's root in order to use relative paths
+// (i.e. "./foo/bar"). TODO Find out if the problem is with incompatible Node
+// versions (16 vs 18).
 chdir(__dirname);
 
-
-
 http.createServer(function (request, response) {
-    if (request.method === "POST") //if client is sending a POST request, log sent data
+    switch (request.method)
     {
-        let data = '';
-        console.log('received POST');
-        request.on('data', chunk => {
-            data += chunk;
-        });
-
-        request.on('end', () => {
-            if (Object.keys(JSON.parse(data)).includes('architecture')) { // Handle Device Descriptions
-                console.log(' --- this is a device description --- ');
-                fileSystem.writeFile('./files/devicedescription.json', data, function (err) {
-                    if (err) return console.log(err);
-                    console.log('--- data written to file devicedescription.json ---');
-                    response.end();
-                });
-            }
-            //save sent json content of manifest to a json file
-            else fileSystem.writeFile('./files/manifest.json', data, function (err) { // Handle Manifest files
-                if (err) return console.log(err);
-                console.log('data written to file manifest.json');
-                response.end(startSearch()); //TODO: Start searching for suitable packages using saved file
-            });
-            response.end();
-        });
+    case "POST": //if client is sending a POST request, log sent data
+        handlePost(request, response);
+        break;
+    case "GET":
+        handleGet(request, response);
+        break;
     }
+}).listen(3000);
 
-    if (request.method === "GET" && request.url === "/") {
+function handleGet(request, response) {
+    if (request.url === "/") {
         console.log("received GET");
         var filePath = path.join(__dirname, 'files/simple.wasm'); //hardcoded filepath to served file
 
@@ -63,8 +50,7 @@ http.createServer(function (request, response) {
         readStream.on('end', function () {
             response.end();
         });
-    }
-    else if (request.method === 'GET') {
+    } else {
         var filePath = path.join(__dirname, request.url);
         console.log(filePath);
 
@@ -92,8 +78,31 @@ http.createServer(function (request, response) {
         readStream.on('end', function () {
             response.end(console.log('ended readstream, listening'));
         });
-
-
     }
+}
 
-}).listen(3000);
+function handlePost(request, response) {
+    let data = '';
+    console.log('received POST');
+    request.on('data', chunk => {
+        data += chunk;
+    });
+
+    request.on('end', () => {
+        if (Object.keys(JSON.parse(data)).includes('architecture')) { // Handle Device Descriptions
+            console.log(' --- this is a device description --- ');
+            fileSystem.writeFile('./files/devicedescription.json', data, function (err) {
+                if (err) return console.log(err);
+                console.log('--- data written to file devicedescription.json ---');
+                response.end();
+            });
+        }
+        //save sent json content of manifest to a json file
+        else fileSystem.writeFile('./files/manifest.json', data, function (err) { // Handle Manifest files
+            if (err) return console.log(err);
+            console.log('data written to file manifest.json');
+            response.end(startSearch()); //TODO: Start searching for suitable packages using saved file
+        });
+        response.end();
+    });
+}
