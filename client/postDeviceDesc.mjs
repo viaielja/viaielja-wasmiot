5613ea4afcbbba900a453103fbd5f06055995c0f
Juho Tarkkanen <juho.a.tarkkanen@jyu.fi>
2023-01-17 16:24:05 +0200

    Change client-scripts to match a ReStFuL API

--- client/postDeviceDesc.mjs
+++ client/postDeviceDesc.mjs
@@ -1,6 +1,9 @@
-const fetch = require('node-fetch'); //used for 
-const manifest = require('./devicedescription.json'); //hard-coded json file with manifest of required functionality
-const fs = require('fs');
+// Require-importing failed and suggested to use this syntax (some
+// EcmaScript-issue).
+import fetch from 'node-fetch' //used for 
+// Import failed so used this ->
+// https://stackoverflow.com/questions/70106880/err-import-assertion-type-missing-for-import-of-json-file
+import manifest from './devicedescription.json' assert { type:  "json" } //hard-coded json file with manifest of required functionality
 
 
 
@@ -8,10 +11,10 @@ const fs = require('fs');
 sends a fetch POST request to a server containing manifest in the body
 */
 (async function postFile() {
-    const rawResponse = await fetch('http://localhost:3000/.', {
+    const rawResponse = await fetch('http://localhost:3000/file/device', {
         method: 'POST',
         headers: {
-      'Content-Type': 'text/html'
+      'Content-Type': 'application/html'
         },
         body: JSON.stringify(manifest)
       }).then(data => console.log(data)); //log the response sent by the server
