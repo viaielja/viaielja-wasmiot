7f6061ae770d992b1938cd525fb7bd550d5b4d7b
Ville Heikkilä <ville.heikkila@tuni.fi>
2023-04-20 14:36:01 +0300

    Fix constant export syntax (#26)

--- fileserv/constants.js
+++ fileserv/constants.js
@@ -6,8 +6,13 @@ const mongo_host = process.env.CONFIG_MONGODB_HOST || "mongo";
 const mongo_port = process.env.CONFIG_MONGODB_PORT || "27017";
 const mongo_user = process.env.CONFIG_MONGODB_ADMINUSERNAME;
 const mongo_pass = process.env.CONFIG_MONGODB_ADMINPASSWORD;
-export const MONGO_URI = `mongodb://${mongo_user}:${mongo_pass}@${mongo_host}:${mongo_port}/`;
+const MONGO_URI = `mongodb://${mongo_user}:${mongo_pass}@${mongo_host}:${mongo_port}/`;
 
 const public_host = process.env.CONFIG_PUBLIC_HOST || `http://${require("os").hostname()}`;
 const public_port = process.env.CONFIG_PUBLIC_PORT || "3000";
-export const PUBLIC_BASE_URI = `${public_host}:${public_port}/`;
+const PUBLIC_BASE_URI = `${public_host}:${public_port}/`;
+
+module.exports = {
+    MONGO_URI,
+    PUBLIC_BASE_URI
+};
