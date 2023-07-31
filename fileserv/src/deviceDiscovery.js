36c90421e9238250640635d56b204839d64eea40
Juho Tarkkanen <juho.a.tarkkanen@jyu.fi>
2023-07-31 14:46:49 +0000

    Device discovery refactors and changes
    * Add more device managing responsibility to the mDNS "wrapper"
    * Make a first draft for device health checks
    * Remove database operations of devices for now (would need to keep mDNS and DB in sync)

--- fileserv/src/deviceDiscovery.js
+++ fileserv/src/deviceDiscovery.js
@@ -1,14 +1,15 @@
-const http = require('http');
-
 const bonjour = require("bonjour-service");
-const { DEVICE_DESC_ROUTE } = require("../constants.js");
+const { DEVICE_DESC_ROUTE, DEVICE_HEALTH_ROUTE } = require("../constants.js");
 
 
 /**
- * Helper for using mDNS to discover and handle __Wasm-IoT__ -devices in the
- * network.
+ * Interface to list available devices and send them messages.
+ *
+ * "Device" is used as a term for a thing running the Wasm-IoT supervisor.
+ *
+ * Uses mDNS for discovering devices.
  */
-class DeviceDiscovery {
+class DeviceManager {
     /**
      * Initialize fields needed in querying for IoT-devices.
      * @param type The type of mDNS service to search for.
@@ -21,175 +22,211 @@ class DeviceDiscovery {
         this.bonjourInstance = new bonjour.Bonjour();
         this.browser = null;
         this.database = database;
+        // This is where the needed data on available devices is stored TODO: is
+        // the database even needed then?
+        this.devices = [];
         this.queryOptions = { type };
     }
 
     /**
-     * Start browsing for the services and add their descriptions to database as
-     * needed. Also set event listeners for browser.
-     * TODO browse for http services under the wasmiot-domain instead?
+     * Start browsing for the services and save their descriptions to database as
+     * needed. Also set handling when a "well behaving" device leaves the
+     * discovery's reach.
      */
-    run() {
-        async function onFound(service) {
-            // TODO/FIXME: A device is no longer "found" on mDNS after this but the
-            // description-query-chain might fail ending up with nothing but nulls
-            // in the database...
-            console.log(`Found '${service.name}'! `, service);
-            this.saveDeviceData(service);
-            this.logServices();
-        }
-        // This is needed in order to refer to outer "this" instead of the
-        // bonjour-browser-"this" inside the callback...
-        onFound = onFound.bind(this);
-
-        function onDown(service) {
-            // Remove service from database once it leaves/"says goodbye".
-            console.log("Service emitted 'goodbye' :", service);
-            // NOTE: Using IP-addresses as filter for deletion.
-            this.database.delete("device", { addresses: service.addresses });
-            this.logServices();
-        }
-        onDown = onDown.bind(this);
-
+    startDiscovery() {
         // Bonjour/mDNS sends the queries on its own; no need to send updates
         // manually.
         this.browser = this.bonjourInstance.find(this.queryOptions);
  
-        this.browser.on("up", onFound);
-        this.browser.on("down", onDown);
+        // This is needed in order to refer to outer "this" instead of the
+        // bonjour-browser-"this" inside the callback...
+        this.browser.on("up", this.#saveDevice.bind(this));
+        this.browser.on("down", this.#forgetDevice.bind(this));
 
-        // Log available devices every minute.
-        setInterval(this.logServices.bind(this), 60000);
+        // Check the status of the services every 2 minutes. (NOTE: This is
+        // because the library used does not seem to support re-querying the
+        // services on its own).
+        setInterval(this.healthCheck.bind(this), 120000);
 
         console.log("mDNS initialized; searching for hosts with ", this.queryOptions);
     }
 
-    logServices() {
-        let date = new Date();
-        console.log("[", date, "] current services: ", this.browser.services.map(x => x.name)); 
-    }
-
     /**
-     * Query information like WoT-description and platform info to be saved into the
-     * database from the device.
-     * @param {*} serviceData Object containing needed data of the device discovered via mDNS.
+     * Transform the data of a service into usable device data and query needed
+     * additional information like WoT-description and platform.
+     * @param {*} serviceData Object containing needed data of the service discovered via mDNS.
      */
-    async saveDeviceData(serviceData) {
-        // Check for duplicate service
-        let device_doc = (await this.database.read("device", { name: serviceData.name }))[0];
-
-        // Check if __all__ the required information has been received earlier.
-        // NOTE: This is not a check to prevent further actions if device already
-        // simply exists in database.
-        if (device_doc
-            && device_doc.hasOwnProperty("description") && device_doc.description !== null
-            && device_doc.description.hasOwnProperty("platform") && device_doc.description.platform !== null
-        ) {
-            console.log(`The device named '${device_doc.name}' is already in the database!`);
+    #saveDevice(serviceData) {
+        let newDevice = this.#addNewDevice(serviceData);
+        // Check for duplicate or unknown (i.e., non-queried) device.
+        if (!newDevice) {
+            console.log(`Service '${serviceData.name}' is already known!`);
             return;
         }
 
-        // Insert or get new device into database for updating in GET-callbacks.
-        let newId;
-        if (!device_doc) {
-            try {
-                newId = (await this.database.create("device", [serviceData])).insertedIds[0];
-                console.log("Added new device: ", serviceData);
-            } catch (e) {
-                console.error(e.message);
-            }
-        } else {
-            newId = device_doc._id;
-        }
-
-        let requestOptions = { host: serviceData.addresses[0], port: serviceData.port, path: DEVICE_DESC_ROUTE };
-
-        console.log("Querying service's description(s) via HTTP... ", requestOptions);
-
-        // The returned description should follow the common schema for WasmIoT TODO
-        // Perform validation.
-        this.queryDeviceData(requestOptions, (data) => {
-            let deviceDescription;
-            try {
-                deviceDescription = JSON.parse(data);
-            } catch (error) {
-                console.log("Error - description JSON is malformed: ", error)
-                return;
-            }
-
-            // Save description in database. TODO Use some standard way to
-            // interact with descriptions (validations, operation,
-            // contentType, security etc)?.
-            this.database.update(
-                "device",
-                { _id: newId },
-                { description: deviceDescription }
-            );
-            console.log(`Adding device description for '${serviceData.name}'`);
-        });
+        this.#deviceIntroduction(newDevice);
     }
 
     /**
-     * Query device __for 'data'-events__ and if fails, remove from mDNS-cache.
-     * NOTE: This is for device introduction and not for general queries!
-     * @param {*} options Options to use in the GET request of `http.get` including the URL.
-     * @param {*} callback What to do with the data when request ends.
+     * Based on found service, create a device entry of it if the device is not
+     * already known.
+     * @param {*} serviceData 
+     * @returns The device entry created or null if the device is already fully
+     * known.
      */
-    queryDeviceData(options, callback) {
-        /**
-         * Find and forget the service in question whose advertised host failed to
-         * answer to HTTP-GET.
-         */
-        function handleError(responseOrHttpGetError) {
-            let statusMsg = responseOrHttpGetError.statusCode ? ": Status " + responseOrHttpGetError.statusCode: ".";
-            console.log(`Service at '${options.host}${options.path}' failed to respond${statusMsg}`);
-
-            let faultyServices = this.browser
-                .services
-                .filter(service => service.addresses.includes(options.host));
-
-            if (faultyServices.length > 0) {
-                // FIXME/TODO Bonjour keeps the device saved, but it should forget it
-                // here because the device is not functional. Current library does
-                // not seem to support removing the found service...
-                console.log("UNIMPLEMENTED/TODO: Should forget the faulty devices: ", faultyServices);
-            } else {
-                console.log(`Did not find any devices with advertised IP ${options.host} in currently known mDNS devices`);
+    #addNewDevice(serviceData) {
+        if (!this.devices[serviceData.name]) {
+            // Transform the service into usable device data.
+            this.devices[serviceData.name] = {
+                // Devices are identified by their "fully qualified" name.
+                id: serviceData.name,
+                communication: {
+                    addresses: serviceData.addresses,
+                    port: serviceData.port,
+                }
+            };
+        } else {
+            let device = this.devices[serviceData.name];
+            if (device.description && device.description.platform) {
+                return null;
             }
-            return null;
         }
-        // Sigh...
-        handleError = handleError.bind(this);
 
-        http.get(options, (res) => {
-            if (res.statusCode !== 200) {
-                handleError(res);
-            } else {
-                let rawData = '';
-                res.on('data', (chunk) => { rawData += chunk; });
-                res.on('end', () => callback(rawData));
-            }
-        })
-        .on("error", handleError);
+        return this.devices[serviceData.name];
     }
 
     /**
-     * Refresh (i.e., reset and rerun) device discovery so that devices already
-     * discovered and running will be discovered again. TODO: Implement probing
-     * of devices that are still alive and change into "refresh" (i.e., update
-     * address based on name and forget missing ones in scanner) instead of
-     * "reset" (i.e., reinitialize scanning entirely).
-     *
-     * NOTE: Throws if re-initializing fails.
+     * Perform tasks for device introduction.
+     * 
+     * Query device __for 'data'-events__ on its description path and if fails,
+     * remove from mDNS-cache.
+     * @param {*} device The device to introduce.
+     */
+    async #deviceIntroduction(device) {
+        const handleIntroductionErrorBound = (function handleIntroductionError(errorMsg) {
+                // Find and forget the service in question whose advertised
+                // host failed to answer to HTTP-GET.
+                console.log(" Error in device introduction: ", errorMsg);
+                this.#forgetDevice(device.id);
+            })
+            .bind(this);
+
+        // FIXME: Using first address but might want to try all available.
+        let url = new URL(`http://${device.communication.addresses[0]}:${device.communication.port}`);
+        url.pathname = device.deviceDescriptionPath || DEVICE_DESC_ROUTE;
+
+        console.log("Querying device description via GET", url.toString());
+
+        let res = await fetch(url);
+        if (res.status !== 200) {
+            handleIntroductionErrorBound(
+                `${JSON.stringify(device.communication, null, 2)} responded ${res.status} ${res.statusText}`
+            );
+            return;
+        }
+
+        let deviceDescription;
+        try {
+            // The returned description should follow the common schema for
+            // WasmIoT TODO Perform validation.
+            deviceDescription = await res.json();
+        } catch (error) {
+            handleIntroductionErrorBound(`description JSON is malformed: ${error}`);
+            return;
+        }
+
+        this.devices[device.id].description = deviceDescription;
+
+        console.log(`Added description for device '${device.id}'`);
+
+        // Do an initial health check on the new device.
+        this.healthCheck(device.id);
+    }
+
+    /**
+     * Force refresh of device discovery so that devices already discovered and
+     * running will be discovered again.
+     * 
+     * @throws If re-initializing scanning fails.
      */
     refresh() {
         this.destroy();
+
         this.bonjourInstance = new bonjour.Bonjour();
-        this.run();
+        this.startDiscovery();
     }
 
     destroy() {
         this.bonjourInstance.destroy();
+        this.devices = {};
+    }
+
+    /**
+     * Check and update "health" of currently known devices.
+     * TODO: This should be restful in that the one running in interval should
+     * not clash with direct calls.
+     * @param {*} deviceId Id of the device to check. If not given, check all.
+     */
+    async healthCheck( deviceId) {
+        let devices = deviceId ? [this.devices[deviceId]] : this.devices;
+
+        let date = new Date();
+        let healthChecks = devices.map(x => ({
+                device: x.id,
+                // Gather promises to be awaited.
+                check: this.#healthCheckDevice(x),
+                timestamp: date,
+            }));
+
+        for (let x of healthChecks) {
+            try {
+                let health = await x.check;
+                this.devices[x.device].health = {
+                    report: health,
+                    timeOfQuery: x.timestamp,
+                };
+            } catch (e) {
+                console.log(`Forgetting device '${x.device}' with health problems:`, e);
+                this.#forgetDevice(x.device);
+            }
+        }
+
+        console.log("[", date, "] current number of devices: ", Object.keys(this.devices).length); 
+    }
+
+
+    /**
+     * Forget a device based on an identifier or one derived from mDNS service data.
+     * @param {*} x The string-identifier or service data of the device to forget. 
+     */
+    #forgetDevice(x) {
+        let key = null;
+        if (typeof x === "string") {
+            key = x;
+        } else {
+            console.log(`Service '${service.name}' seems to have emitted 'goodbye'`);
+            // Assume the service data from mDNS is used to remove a device.
+            key = x.name;
+        }
+
+        delete this.devices[key];
+    }
+
+
+    /**
+     * Query the device for health
+     * @param {*} device The device to query.
+     * @throws If there were error querying the device.
+     */
+    async #healthCheckDevice(device) {
+        let url = new URL(`http://${device.communication.addresses[0]}:${device.communication.port}/${device.healthCheckPath || DEVICE_HEALTH_ROUTE}`);
+        let res = await fetch(url);
+
+        if (res.status !== 200) {
+            throw `${url.toString()} responded ${res.status} ${res.statusText}`
+        }
+
+        return res.json();
     }
 }
 
@@ -199,6 +236,6 @@ class MockDeviceDiscovery {
 }
 
 module.exports = {
-    DeviceDiscovery,
+    DeviceDiscovery: DeviceManager,
     MockDeviceDiscovery,
 };
