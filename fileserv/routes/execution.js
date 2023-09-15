9732c1fc3cb74ff9f97de03daf7c4652bbfd1227
Juho Tarkkanen <juho.a.tarkkanen@jyu.fi>
2023-09-15 14:01:09 +0000

    Hack together a loop to follow execution URLs
    This way the /execute request responds with the actual end result.
    This might eventually need two-way communication for client and server
    (e.g. because of timeouts)
    but works for now

--- fileserv/routes/execution.js
+++ fileserv/routes/execution.js
@@ -30,12 +30,70 @@ const execute = async (request, response) => {
         if (request.file) {
             args.files = [request.file.path];
         }
-        let startResponse = await orchestrator.schedule(deployment, args);
-        if (!startResponse.ok) {
-            throw JSON.stringify(await startResponse.json());
+        let execResponse = await orchestrator.schedule(deployment, args);
+        if (!execResponse.ok) {
+            throw JSON.stringify(await execResponse.json());
         }
-        let startResponseJson = await startResponse.json();
-        response.json(startResponseJson);
+        // Recursively seek the end of the execution chain in order respond with
+        // the end result of all steps in the executed sequence.
+        let tries = 0;
+        let depth = 0;
+        let statusCode = 500;
+        let result = new utils.Error("undefined error");
+        while (true) {
+            let json;
+            try {
+                json = await execResponse.json();
+            } catch (e) {
+                result = new utils.Error("parsing result to JSON failed: " + json.statusText);
+                break;
+            }
+
+            // TODO: This is just temporary way to check for result. Would be
+            // better that supervisor responds with error code, not 200.
+            if (json.result) {
+                if (json.status !== "error") {
+                    // Check if the result is a URL to follow...
+                    try {
+                        url = new URL(json.result);
+                        depth += 1;
+                    } catch (e) {
+                        // Assume this is the final result.
+                        console.log("Result found!", JSON.stringify(json, null, 2));
+                        result = json.result;
+                        statusCode = 200;
+                        break;
+                    }
+                }
+            } else if (json.error) {
+                result = new utils.Error(json.error);
+                break;
+            } else if (json.resultUrl) {
+                url = json.resultUrl;
+                depth += 1;
+            }
+
+            options = { method: "GET" };
+
+            console.log(`(Try ${tries}, depth ${depth}) Fetching result from: ${url}`);
+            execResponse = await fetch(url, options);
+
+            if (!execResponse.ok) {
+                // Wait for a while, if the URL is not yet available.
+                if (execResponse.status == 404 && depth < 10) {
+                    await new Promise(resolve => setTimeout(resolve, 1000));
+                } else {
+                    result = new utils.Error("fetching result failed: " + execResponse.statusText);
+                    break;
+                }
+            }
+
+            tries += 1;
+        }
+
+        response
+            .status(statusCode)
+            .json(result);
     } catch (e) {
         response
             .status(500)
