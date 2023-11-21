1e72808906b2c8dd8ee604fdec11363f7066eaf2
Juho Tarkkanen <juho.a.tarkkanen@jyu.fi>
2023-11-21 18:08:32 +0200

    Example: Make a single script for trying datalist and delete the others

--- example/datalist/post_deploy.sh
+++
@@ -1,4 +0,0 @@
-# Request orchestrator to deploy work according to the deployment created
-# earlier identified by DEPLOYMENT_ID argument.
-DEPLOYMENT_ID=$1
-curl -X POST http://localhost:3000/file/manifest/${DEPLOYMENT_ID} --header "Content-Type: application/json"
--- example/datalist/post_manifest.sh
+++
@@ -1,2 +0,0 @@
-# Create a deployment entry at the orchestrator.
-curl -X POST http://localhost:3000/file/manifest --header "Content-Type: application/json" --data "@./manifest.json"
