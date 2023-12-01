3fc51487af73fdcfa0d486d5bac385fa52ae1771
Juho Tarkkanen <50268629+trkks@users.noreply.github.com>
2023-12-01 12:13:54 +0000

    Client: Add module show -command and fix API desc to match

--- client/cli/src/cli-module.ts
+++ client/cli/src/cli-module.ts
@@ -1,4 +1,8 @@
+import { readFile } from "node:fs/promises";
+
 import { Command } from "commander";
+
+import { DefaultService as Api } from "../generatedApiClient";
 
 
 const program = new Command();
@@ -6,6 +10,19 @@ const program = new Command();
 program
     .command("create", "Create a new module")
     .command("desc", "Describe an existing module");
+
+program
+    .command("show")
+    .description("Return information related to modules")
+    .option("-m --module <module-id-string>", "ID of a single module")
+    .action(async (options, _) => {
+        const result = 
+            options.module
+            ? await Api.getFileModule1(options.module)
+            : await Api.getFileModule();
+
+        console.log(JSON.stringify(result, null, 4));
+    });
 
 program
     .showHelpAfterError()
