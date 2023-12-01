f8b633a60f9ea6953a00fd986d6b81c01d3c7c20
Juho Tarkkanen <50268629+trkks@users.noreply.github.com>
2023-12-01 09:47:57 +0000

    Client: Implement module description based on file

--- client/cli/src/cli-module-desc.ts
+++ client/cli/src/cli-module-desc.ts
@@ -1,12 +1,21 @@
+import { readFile } from "node:fs/promises";
+
 import { Command } from "commander";
+
+import { DefaultService as Api } from "../generatedApiClient";
 
 
 const program = new Command();
 
 program
     .argument("<module-id-string>", "ID of the module")
-    .action(async () => {
-        console.error("Not implemented");
+    .argument("<description-file>", "Path to JSON file describing functions of the module")
+    .action(async (id, descPath) => {
+        const descObj = JSON.parse(
+            await readFile(descPath, "utf8")
+        );
+        const result = await Api.postFileModuleUpload(id, descObj);
+        console.log(JSON.stringify(result, null, 4));
     });
 
 program
