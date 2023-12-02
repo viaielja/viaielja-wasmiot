20ea42fe79e3fa926a19e5caf6b669a583c98d05
Juho Tarkkanen <50268629+trkks@users.noreply.github.com>
2023-12-02 11:19:46 +0000

    Client: Device: Add commands; Update schema based on orch response

--- client/cli/src/cli-device.ts
+++ client/cli/src/cli-device.ts
@@ -1,13 +1,32 @@
 import { Command } from "commander";
 
+import { DefaultService as Api } from "../generatedApiClient";
+
 
 const program = new Command();
+
+program
+    .command("show")
+    .description("Return information related to devices")
+    .action(async () => {
+        const result = await Api.getFileDevice();
+        console.log(JSON.stringify(result, null, 4));
+    });
 
 program
     .command("scan")
     .description("Scan for device advertisements")
     .action(async () => {
-        console.log("I'm scanning");
+        await Api.postFileDeviceDiscoveryReset();
+        console.log("Rescan started");
+    });
+
+program
+    .command("rm")
+    .description("Delete all devices")
+    .action(async () => {
+        const result = await Api.deleteFileDevice();
+        console.log(JSON.stringify(result, null, 4));
     });
 
 program
