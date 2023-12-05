3c9244ddf7d905c48edf2f430aa625079439e73d
Juho Tarkkanen <50268629+trkks@users.noreply.github.com>
2023-12-05 11:57:57 +0000

    Docs: Make the test .wasm module's exports visible

--- client/testData/wasm/abc/src/lib.rs
+++ client/testData/wasm/abc/src/lib.rs
@@ -24,6 +24,7 @@ fn handle_error(e: MountReadFailed) -> i32 {
 }
 
 /// Demonstrates reading from files and returning signed 32bit integer.
+#[no_mangle]
 pub fn a(p0: u32, p1: f32) -> i32 {
     let Ok(dbytes) = std::fs::read("deployFile") else { return handle_error(MountReadFailed::Deploy) };
     let Ok(ebytes) = std::fs::read("execFile") else { return handle_error(MountReadFailed::Exec) };
@@ -39,11 +40,13 @@ pub fn a(p0: u32, p1: f32) -> i32 {
 }
 
 /// Demonstrates returning a 32bit floating point value.
+#[no_mangle]
 pub fn b() -> f32 {
     4.2
 }
 
 /// Demonstrates writing to a file and returning unsigned 32bit integer.
+#[no_mangle]
 pub fn c() -> u32 {
     // Write something into the file to indicate it is really manipulated.
     let Ok(_) = std::fs::write("outFile", b"42") else { return handle_error(MountReadFailed::Out) as u32 };
