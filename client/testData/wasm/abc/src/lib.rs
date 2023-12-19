86544744eb441adf4c1fef1383e5b85a5b15ed76
Juho Tarkkanen <50268629+trkks@users.noreply.github.com>
2023-12-19 12:15:58 +0200

    Client: Fix test-wasm description and update its return values

--- client/testData/wasm/abc/src/lib.rs
+++ client/testData/wasm/abc/src/lib.rs
@@ -8,26 +8,31 @@
 
 use std::collections::hash_map::DefaultHasher;
 use std::hash::Hasher;
+use std::fs;
 
+
+const DEPLOY_FILE_NAME: &str = "deployFile";
+const EXECUTE_FILE_NAME: &str = "execFile";
+pub const OUTPUT_FILE_NAME: &str = "outFile";
 
 #[derive(Debug)]
 enum MountReadFailed {
-    Deploy = -1,
-    Exec = -2,
+    Deploy = -1337,
+    Exec = -2337,
     Out = 404,
 }
 
-fn handle_error(e: MountReadFailed) -> i32 {
-    eprintln!("Reading a mount-file failed: {:?}", e);
+fn handle_error(error: std::io::Error, eenum: MountReadFailed) -> i32 {
+    eprintln!("Error: Reading a mount-file ({:?}) failed: {:?}", eenum, error);
 
-    e as i32
+    eenum as i32
 }
 
 /// Demonstrates reading from files and returning signed 32bit integer.
 #[no_mangle]
 pub fn a(p0: u32, p1: f32) -> i32 {
-    let Ok(dbytes) = std::fs::read("deployFile") else { return handle_error(MountReadFailed::Deploy) };
-    let Ok(ebytes) = std::fs::read("execFile") else { return handle_error(MountReadFailed::Exec) };
+    let dbytes = match fs::read(DEPLOY_FILE_NAME)  { Ok(x) => x, Err(e) => return handle_error(e, MountReadFailed::Deploy) };
+    let ebytes = match fs::read(EXECUTE_FILE_NAME) { Ok(x) => x, Err(e) => return handle_error(e, MountReadFailed::Exec) };
     // Do something with the files to indicate they are really read.
     let mut hasher = DefaultHasher::new();
     hasher.write(&dbytes);
@@ -49,7 +54,8 @@ pub fn b() -> f32 {
 #[no_mangle]
 pub fn c() -> u32 {
     // Write something into the file to indicate it is really manipulated.
-    let Ok(_) = std::fs::write("outFile", b"42") else { return handle_error(MountReadFailed::Out) as u32 };
+    match fs::write(OUTPUT_FILE_NAME, b"42") { Ok(_) => {}, Err(e) => return handle_error(e, MountReadFailed::Out) as u32 };
 
-    u32::MAX
+    // The Wasm-integer is a _signed_ 32bit so cut the unsigned short.
+    i32::MAX as u32
 }
