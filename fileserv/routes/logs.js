d2ed3b7cd17b039f22b5f48db8c82ecfd5d5528d
Teemu Autto <teemu@rautakuu.org>
2024-06-17 04:01:18 +0300

    Use `after` when fetching logs

--- fileserv/routes/logs.js
+++ fileserv/routes/logs.js
@@ -38,10 +38,14 @@ const createSupervisorLogs = async (req, res) => {
  * Example: /logs?date=2021-01-01T00:00:00.000Z
  */
 const getSupervisorLogs = async (request, response) => {
+    // Make sure we have the index on dateReceived field
+    await collection.createIndex({ dateReceived: 1 });
+
     let filterRule = {};
-    if (request.query.date) {
+    if (request.query.after) {
+        console.log("Getting logs after date: ", new Date(request.query.after));
         // Check if date is provided, if so, get logs after that date,
-        filterRule = { dateReceived: { $gt: new Date(request.query.date) } };
+        filterRule = { dateReceived: { $gt: new Date(request.query.after) } };
     }
     const logs = await collection.find(filterRule).toArray();
     response.json(logs);
